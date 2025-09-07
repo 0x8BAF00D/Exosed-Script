b1b4b102dee0cda9141c2766c1715c5f9e44e5a5
0x8BAF00D <peopleman1337@outlook.com>
2025-09-07 15:17:15 +0300

    Update Exosed Hub.lua

--- Exosed Hub.lua
+++ Exosed Hub.lua
@@ -1,7 +1,7 @@
 
 local Library = loadstring(game:HttpGet("https:///raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library:CreateLib("Exosed Hub", "RJTheme3")
+local Window = Library:CreateLib("Exosed Hub", "RJTheme2")
 
 local Tab = Window:NewTab("Functonal")
 
@@ -14,3 +14,4 @@ end)
 Section:NewSlider2("JumpPower", "Slider Info", 100, 0, function(p)
 	game.Players.LocalPlayer.Character.Humanoid.JumpPower = p
 end)
+
